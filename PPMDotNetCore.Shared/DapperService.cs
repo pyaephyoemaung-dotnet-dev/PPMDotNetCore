c01991b990082333b4f37d6de8d46363615f5184
pyaephyoemaung10 <p666766@gmail.com>
2024-05-05 08:57:47 +0630

    Dapper_Custome_Service

---
+++ PPMDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PPMDotNetCore.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<Kai> Query<Kai>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var lst = db.Query<Kai>(query, param).ToList();
+            return lst;
+        }
+        public Kai QueryFirstOrDefault<Kai>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var item = db.Query<Kai>(query, param).FirstOrDefault();
+            return item!;
+        }
+        public int Execute(string query , object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var result = db.Execute(query, param);
+            return result;
+        }
+    }
+}
