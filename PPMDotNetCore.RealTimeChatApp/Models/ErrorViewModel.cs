d3b45b7b41e121fff67705e1a671098e431bbf48
pyaephyoemaung10 <p666766@gmail.com>
2024-07-14 16:24:17 +0630

    [Completed]RealTimeChatApp

---
+++ PPMDotNetCore.RealTimeChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PPMDotNetCore.RealTimeChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
