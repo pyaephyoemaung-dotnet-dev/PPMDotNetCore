5c5b41edb28230481f4d5906236f0d934d3fa53a
pyaephyoemaung10 <p666766@gmail.com>
2024-07-14 23:19:29 +0630

    [Completed]ChartJsRadarChart

--- PPMDotNetCore.MVCChartApp/Controllers/ChartJsController.cs
+++ PPMDotNetCore.MVCChartApp/Controllers/ChartJsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PPMDotNetCore.MVCChartApp.Models;
 
 namespace PPMDotNetCore.MVCChartApp.Controllers
 {
@@ -6,7 +7,20 @@ namespace PPMDotNetCore.MVCChartApp.Controllers
     {
         public IActionResult RadarChart()
         {
-            return View();
+            ChartJsRadarChartModel model = new ChartJsRadarChartModel();
+            model.Series = new List<string>()
+            {
+                "Eating",
+                "Drinking",
+                "Sleeping",
+                "Designing",
+                "Coding",
+                "Cycling",
+                "Running"
+            };
+            model.data1 = new List<int> { 65, 59, 90, 81, 56, 55, 40};
+            model.data2 = new List<int> { 28, 48, 40, 19, 96, 27, 100 };
+            return View(model);
         }
     }
 }
---
+++ PPMDotNetCore.MVCChartApp/Models/ChartJsRadarChartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PPMDotNetCore.MVCChartApp.Models
+{
+    public class ChartJsRadarChartModel
+    {
+        public List<int> data1 { get; set; }
+        public List<int> data2 { get; set; }
+        public List<string> Series { get; set; }
+    }
+}
