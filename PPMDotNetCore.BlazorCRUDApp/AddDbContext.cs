d284c5629c5ec2bcd1e36621d421ff633b923487
pyaephyoemaung10 <p666766@gmail.com>
2024-07-28 01:30:40 +0630

    [Completed]BlazorServerCRUDApp

---
+++ PPMDotNetCore.BlazorCRUDApp/AddDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PPMDotNetCore.BlazorCRUDApp.Model;
+
+namespace PPMDotNetCore.BlazorCRUDApp
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        //}
+        public DbSet<BlogsModel> Blogs { get; set; }
+    }
+}
