4f352f8c5564e128c551a4c76eff5e70eafb24d8
pyaephyoemaung10 <p666766@gmail.com>
2024-07-29 23:41:14 +0630

    [Completed]LoginMiddleware

---
+++ PPMDotNetCore.LoginMiddleware/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PPMDotNetCore.LoginMiddleware.EFDbModels;
+
+namespace PPMDotNetCore.LoginMiddleware.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public LoginController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel user)
+        {
+            var item = _db.Users.FirstOrDefault(x => x.UserName == user.UserName && x.Password == user.Password);
+            if (item is null) return View();
+            string sessionId = Guid.NewGuid().ToString();
+            DateTime sessionExp = DateTime.Now.AddMinutes(1);
+            CookieOptions option = new CookieOptions();
+            option.Expires = DateTime.Now.AddSeconds(10);
+            Response.Cookies.Append("UserId", item.UserId, option);
+            Response.Cookies.Append("SessionId", sessionId, option);
+            await _db.Login.AddAsync(new LoginModel
+            {
+                SessionId = sessionId,
+                UserId = item.UserId,
+                SessionExp = sessionExp
+            });
+            await _db.SaveChangesAsync();
+            return Redirect("/Home");
+        }
+    }
+}
---
+++ PPMDotNetCore.LoginMiddleware/EFDbModels/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PPMDotNetCore.LoginMiddleware.EFDbModels;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<LoginModel> Login { get; set; }
+}
+[Table("Table_Login")]
+public class LoginModel
+{
+    [Key]
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string SessionId { get; set; }
+    public DateTime SessionExp { get; set; }
+}
+[Table("Table_Client")]
+public class UserModel
+{
+    [Key]
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public int Password { get; set; }
+}    
