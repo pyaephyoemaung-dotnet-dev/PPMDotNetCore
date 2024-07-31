9aafe8d11505f2295d3173eb1c4a61f4ab4f3119
pyaephyoemaung10 <p666766@gmail.com>
2024-07-31 23:07:03 +0630

    [Completed]BlazorWebAssemblyApp

---
+++ PPMDotNetCore.WebAssemblyApp/Model/BlogsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PPMDotNetCore.WebAssemblyApp.Model;
+public class BlogsModel
+{
+    public string BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
+//public record BlogEntity(int BlogId, string BlogTitle, string BlogAuthor,string BlogContent);
