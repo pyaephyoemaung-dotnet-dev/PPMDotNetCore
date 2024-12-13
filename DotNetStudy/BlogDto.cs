6708b8259fd2937a2b49f84c86b181d28eba8d5c
pyaephyoemaung10 <p666766@gmail.com>
2024-12-13 21:20:09 +0630

    [Completed]RunTimeIssue

---
+++ DotNetStudy/BlogDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetStudy;
+[Table("tbl_blog")]
+public class BlogDto
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
