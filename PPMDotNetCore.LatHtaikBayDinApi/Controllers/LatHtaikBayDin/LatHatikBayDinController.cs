3d848a48125f39a3dbf82520617f03df3fc19dcd
pyaephyoemaung10 <p666766@gmail.com>
2024-12-20 23:09:58 +0630

    [Completed]LatHtaunkBayDinApitesting

---
+++ PPMDotNetCore.LatHtaikBayDinApi/Controllers/LatHtaikBayDin/LatHatikBayDinController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace PPMDotNetCore.LatHtaikBayDinApi.Controllers.LatHtaikBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHatikBayDinController : ControllerBase
+    {
+        private async Task<LatHatikBayDin> GetDataAsync()
+        {
+            string json = await System.IO.File.ReadAllTextAsync("LatHatikBayDin.json");
+            var model = JsonConvert.DeserializeObject<LatHatikBayDin>(json);
+            return model!;
+        }
+        [HttpGet]
+        public async Task<IActionResult> QuestionsAsync()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+        [HttpGet]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+        [HttpGet("{questionsNo}/{no}")]
+        public async Task<IActionResult>Answer(int questionsNo,int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionsNo && x.answerNo == no));
+        }
+    }
+}
+
+public class LatHatikBayDin
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
