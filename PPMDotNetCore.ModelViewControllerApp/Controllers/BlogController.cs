18fa8271192b0fb7c87dd4106607cd6fd249fd5d
pyaephyoemaung10 <p666766@gmail.com>
2025-01-09 20:12:06 +0630

    [Completed]ASP.Net MVC Blog-create-process

--- PPMDotNetCore.ModelViewControllerApp/Controllers/BlogController.cs
+++ PPMDotNetCore.ModelViewControllerApp/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PPMDotNetCore.ModelViewControllerApp.Database;
+using PPMDotNetCore.ModelViewControllerApp.Models;
 
 namespace PPMDotNetCore.ModelViewControllerApp.Controllers
 {
@@ -17,6 +18,18 @@ namespace PPMDotNetCore.ModelViewControllerApp.Controllers
         {
             var lst = await _db.Blogs.ToListAsync();
             return View(lst);
+        }
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogCreate(BlogModel blog)
+        {
+            await _db.Blogs.AddAsync(blog);
+            var result = await _db.SaveChangesAsync();
+            return Redirect("/Blog");
         }
     }
 }
